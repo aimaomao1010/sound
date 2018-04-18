1301b8910fe52ee2f7072e8b85745f9f30ad6de2
aimaomao <aimaomao1996@qq.com>
2018-04-18 12:00:48 +0000

    18.4.2018

---
+++ sound.php
@@ -0,0 +1,21 @@
+<?php
+$log_file = 'sound_log.txt';
+$data = $_POST['data'];
+$today = date("Y-m-d H:i:s");
+$str = $today.', '.$data."\n";
+//echo "This is from the server\n";
+//now open the file
+if(file_exists($log_file))
+{
+	$fp=fopen($log_file,"a");
+	fwrite($fp, $str);
+	fclose($fp);
+}
+else
+{
+	$fp = fopen($log_file,"w");
+	fwrite($fp, $str);
+	fclose($fp);
+}
+echo "Data is stored\n";
+?>
